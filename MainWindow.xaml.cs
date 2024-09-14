da16a4e8ad7862f6e3380c1173dd214a9fb41350
Wizxrd <85634781+Wizxrd@users.noreply.github.com>
2024-09-13 23:53:25 -0400

    NextGen initial

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using Newtonsoft.Json.Linq;
+using SRCClient.Models;
+using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Input;
+using System.IO;
+using SRCClient.Views;
+
+namespace SRCClient
+{
+    public partial class MainWindow : Window
+    {
+        public JObject? config;
+        public MainWindow()
+        {
+            LoadFile.DEBUG = true;
+            InitializeComponent();
+            Logger.Wipe();
+            Logger.Debug("SRCClient", "Loading v0.0.1");
+            InitializeConfig();
+            InitializeCursor();
+            InitializeEvents();
+        }
+        // Initializers
+        private void InitializeConfig()
+        {
+            try
+            {
+                config = JObject.Parse(File.ReadAllText(LoadFile.Load("Config", "Config.json")));
+                this.Left = (int?)config["Window"]?["Location"]?["Left"] ?? 0;
+                this.Top = (int?)config["Window"]?["Location"]?["Top"] ?? 0;
+                this.Width = (int?)config["Window"]?["Size"]?["Width"] ?? 1280;
+                this.Height = (int?)config["Window"]?["Size"]?["Height"] ?? 720;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("MainWindow.LoadConfig", ex.ToString());
+            }
+        }
+
+        private void InitializeCursor()
+        {
+            string cursor = config["Cursor"]?.ToString() ?? string.Empty;
+            if (cursor != string.Empty)
+            {
+                this.Cursor = new System.Windows.Input.Cursor(System.Windows.Application.GetResourceStream(new Uri($"pack://application:,,,/Cursors/{cursor}", UriKind.Absolute)).Stream);
+            }
+            else
+            {
+                Logger.Error("MainWindow.SetCursor", "Config.Cursor is null!");
+            }
+        }
+
+        private void InitializeEvents()
+        {
+            this.KeyDown += MainWindowKeyDown;
+        }
+
+        // Events
+
+        private void MainWindowKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftShift))
+            {
+                if (e.Key == Key.S)
+                {
+                    OpenSaveProfileAsWindow();
+                }
+            }
+        }
+
+        // Button Handlers
+
+        private void MenuButtonClick(object sender, RoutedEventArgs e)
+        {
+            MenuPopup.IsOpen = !MenuPopup.IsOpen;
+        }
+
+        private void BorderMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void MinimizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (this.WindowState == WindowState.Maximized)
+                {
+                    MainWindowGrid.Margin = new Thickness(0);
+                    MaximizeButton.Tag = "pack://application:,,,/Images/Maximize.png";
+                    this.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    MainWindowGrid.Margin = new Thickness(5);
+                    MaximizeButton.Tag = "pack://application:,,,/Images/MaximizeMinimize.png";
+                    this.WindowState = WindowState.Maximized;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("MainWindow.MaximizeButtonClick", ex.ToString());
+            }
+        }
+
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveProfileButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void SaveProfileAsButtonClick(object sender, RoutedEventArgs e)
+        {
+            MenuPopup.IsOpen = !MenuPopup.IsOpen;
+            OpenSaveProfileAsWindow();
+        }
+
+        private void LoadProfileButtonClick(Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Core Functions
+
+        private async void OpenSaveProfileAsWindow()
+        {
+            SaveProfileAsWindow saveProfileAsWindow = new SaveProfileAsWindow(this);
+            //await Task.Delay(1);
+            saveProfileAsWindow.Loaded += (s, ev) =>
+            {
+                saveProfileAsWindow.Top = this.Top + (this.ActualHeight - saveProfileAsWindow.ActualHeight) / 2;
+                saveProfileAsWindow.Left = this.Left + (this.ActualWidth - saveProfileAsWindow.ActualWidth) / 2;
+            };
+            saveProfileAsWindow.ShowDialog();
+        }
+    }
+}
---
+++ Models/LoadFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRCClient.Models
+{
+    internal class LoadFile
+    {
+        public static bool DEBUG = true;
+
+        public static string Load(string folderPath, string fileName)
+        {
+            try
+            {
+                string folderDir;
+                string filePath;
+                if (DEBUG)
+                {
+                    string binDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+                    string solutionDir = Directory.GetParent(binDir).FullName;
+                    folderDir = Path.Combine(solutionDir, folderPath);
+                    filePath = Path.Combine(folderDir, fileName);
+                }
+                else
+                {
+                    string exeDir = AppDomain.CurrentDomain.BaseDirectory;
+                    folderDir = Path.Combine(exeDir, folderPath);
+                    filePath = Path.Combine(folderDir, fileName);
+                }
+                if (!Directory.Exists(folderDir))
+                {
+                    Directory.CreateDirectory(folderDir);
+                }
+                if (!File.Exists(filePath))
+                {
+                    File.WriteAllText(filePath, "");
+                }
+                return filePath;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Load", ex.ToString());
+                return string.Empty;
+            }
+        }
+    }
+}
